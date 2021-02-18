e757cc0e912169aa59453f867678276073dc04cd
Love Bhardwaj <lovebhardwaj@Loves-MacBook-Pro.local>
2021-02-18 16:18:09 -0500

    update: move the save token fucntionality from aut to config

--- src/cli/commands/config/config.ts
+++ src/cli/commands/config/config.ts
@@ -1,21 +1,28 @@
-import { clearToken } from '../../../lib/auth/auth';
-import { printRed } from '../../../lib/utils/printToConsole';
+import { clearToken, saveTokenToConfig } from '../../../lib/auth/auth';
+import { printGreen, printRed } from '../../../lib/utils/printToConsole';
 import { appDebugLog } from '../../../lib/utils/debugLogger';
 
 const command = 'config [options]';
-const describe = 'Save the Snyk API token';
+const describe = 'CLI took configurations';
 const builder = {
   'clear-token': {
     describe: 'Clear the saved API token',
+  },
+  'auth-token': {
+    describe: 'Save the Snyk API token',
   },
 };
 
 const handler = (argv: any) => {
   appDebugLog(`Config command with argv: ${JSON.stringify(argv)}`);
+
   if (argv['clear-token']) {
     clearToken();
-    appDebugLog('API token cleared from config');
     return printRed('API token cleared!');
+  } else if (argv['auth-token']) {
+    const token = argv['auth-token'];
+    saveTokenToConfig(token);
+    return printGreen('API token saved to config');
   }
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
 
-import authModule from './cli/commands/auth/auth';
 import configModule from './cli/commands/config/config';
 import processModule from './cli/commands/process/process';
 
 const argv = yargs(process.argv.slice(2))
-  .command(authModule)
   .command(configModule)
   .command(processModule)
   .option('help', {
