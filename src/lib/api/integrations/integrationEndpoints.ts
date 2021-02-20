54b3d9679cc813507c3729e0eee6d5a04b072938
Love Bhardwaj <lovebhardwaj@Loves-MacBook-Pro.local>
2021-02-20 15:32:02 -0500

    Added: integrations endpoints

---
+++ src/lib/api/integrations/integrationEndpoints.ts
@@ -0,0 +1,14 @@
+export default [
+  'list-integrations',
+  'add-new-integration',
+  'update-integration',
+  'delete-credentials',
+  'provision-new-broker-token',
+  'switch-broker-token',
+  'clone-integration',
+  'get-integration-by-type',
+  'import-project',
+  'get-import-job-details',
+  'get-integration-settings',
+  'update-integraion-settings',
+];
