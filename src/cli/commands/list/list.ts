83c01789dacb66a5cbb0e3af82da795c88d140e4
Love Bhardwaj <lovebhardwaj@Loves-MacBook-Pro.local>
2021-03-04 11:41:49 -0500

    refractor: list code to lib for its own

--- src/cli/commands/list/list.ts
+++ src/cli/commands/list/list.ts
@@ -18,6 +18,7 @@ import {
   getTestTable,
   getUserTable,
 } from '../../../lib/list/tables';
+import handler from '../../../lib/list/listHandler';
 
 const command = 'list [options]';
 const describe = 'List API and related endpoints available';
@@ -28,72 +29,6 @@ const builder: yargs.CommandBuilder = {
     alias: 'a',
     string: true,
   },
-};
-
-const handler = (args: any) => {
-  appDebugLog(`List command called with ${JSONify(args)}`);
-
-  if (args[COMMAND_ARGS.API]) {
-    const api = args[COMMAND_ARGS.API];
-
-    switch (api) {
-      case API_SELECTION.GENERAL:
-        const generalTable = getGeneralTable();
-        console.log(generalTable);
-        break;
-      case API_SELECTION.USERS:
-        const userTable = getUserTable();
-        console.log(userTable);
-        break;
-      case API_SELECTION.GROUPS:
-        const groupsTable = getGroupsTable();
-        console.log(groupsTable);
-        break;
-      case API_SELECTION.ORGS:
-        const orgsTable = getOrgTable();
-        console.log(orgsTable);
-        break;
-      case API_SELECTION.INTEGRATIONS:
-        const integTable = getIntegrationTable();
-        console.log(integTable);
-        break;
-      case API_SELECTION.PROJECTS:
-        const projectTable = getProjectTable();
-        console.log(projectTable);
-        break;
-      case API_SELECTION.DEPENDENCIES:
-        const depTable = getDependencyTable();
-        console.log(depTable);
-        break;
-      case API_SELECTION.LICENSES:
-        const licensesTable = getLicenseTable();
-        console.log(licensesTable);
-        break;
-      case API_SELECTION.ENTITLEMENTS:
-        const entitlementsTable = getEntitlementsTable();
-        console.log(entitlementsTable);
-        break;
-      case API_SELECTION.TEST:
-        const testTable = getTestTable();
-        console.log(testTable);
-        break;
-      case API_SELECTION.MONITOR:
-        const monitorTable = getMonitorTable();
-        console.log(monitorTable);
-        break;
-      case API_SELECTION.REPORTING:
-        const reportingTable = getReportingTable();
-        console.log(reportingTable);
-        break;
-      case API_SELECTION.AUDIT:
-        const auditTable = getAuditLogsTable();
-        console.log(auditTable);
-        break;
-    }
-  } else {
-    const apiTable = getApiTable();
-    console.log(apiTable);
-  }
 };
 
 export default { command, describe, builder, handler };
---
+++ src/lib/list/listHandler.ts
@@ -0,0 +1,85 @@
+import { appDebugLog } from '../utils/debugLogger';
+import { COMMAND_ARGS, API_SELECTION } from '../../enums/enums';
+import {
+  getGeneralTable,
+  getUserTable,
+  getGroupsTable,
+  getOrgTable,
+  getIntegrationTable,
+  getProjectTable,
+  getDependencyTable,
+  getLicenseTable,
+  getEntitlementsTable,
+  getTestTable,
+  getMonitorTable,
+  getReportingTable,
+  getAuditLogsTable,
+  getApiTable,
+} from './tables';
+import JSONify from '../utils/JSONify';
+
+export default (args: any) => {
+  appDebugLog(`List command called with ${JSONify(args)}`);
+
+  if (args[COMMAND_ARGS.API]) {
+    const api = args[COMMAND_ARGS.API];
+
+    switch (api) {
+      case API_SELECTION.GENERAL:
+        const generalTable = getGeneralTable();
+        console.log(generalTable);
+        break;
+      case API_SELECTION.USERS:
+        const userTable = getUserTable();
+        console.log(userTable);
+        break;
+      case API_SELECTION.GROUPS:
+        const groupsTable = getGroupsTable();
+        console.log(groupsTable);
+        break;
+      case API_SELECTION.ORGS:
+        const orgsTable = getOrgTable();
+        console.log(orgsTable);
+        break;
+      case API_SELECTION.INTEGRATIONS:
+        const integTable = getIntegrationTable();
+        console.log(integTable);
+        break;
+      case API_SELECTION.PROJECTS:
+        const projectTable = getProjectTable();
+        console.log(projectTable);
+        break;
+      case API_SELECTION.DEPENDENCIES:
+        const depTable = getDependencyTable();
+        console.log(depTable);
+        break;
+      case API_SELECTION.LICENSES:
+        const licensesTable = getLicenseTable();
+        console.log(licensesTable);
+        break;
+      case API_SELECTION.ENTITLEMENTS:
+        const entitlementsTable = getEntitlementsTable();
+        console.log(entitlementsTable);
+        break;
+      case API_SELECTION.TEST:
+        const testTable = getTestTable();
+        console.log(testTable);
+        break;
+      case API_SELECTION.MONITOR:
+        const monitorTable = getMonitorTable();
+        console.log(monitorTable);
+        break;
+      case API_SELECTION.REPORTING:
+        const reportingTable = getReportingTable();
+        console.log(reportingTable);
+        break;
+      case API_SELECTION.AUDIT:
+        const auditTable = getAuditLogsTable();
+        console.log(auditTable);
+        break;
+    }
+  } else {
+    const apiTable = getApiTable();
+    console.log(apiTable);
+  }
+};
